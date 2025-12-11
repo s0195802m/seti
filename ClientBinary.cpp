42e821658a03c84b6b463d85ae951799d2f5a776
s0195802m <margaritadocenko4@gmail.com>
2025-12-12 00:07:37 +0300

    Update ClientBinary.cpp

--- ClientBinary.cpp
+++ ClientBinary.cpp
@@ -60,4 +60,71 @@ int main() {
     }
 
     return 0;
+
+
+    server 
+        #include <iostream>
+#include <fstream>
+#include <windows.h>
+#include <cstring>
+using namespace std;
+
+struct Student {
+    char name[32];
+    int height;
+    int weight;
+};
+
+int main() {
+    setlocale(LC_ALL, "rus");
+
+    const char* fileRequest = "C:/temp/f1.bin";
+    const char* fileAnswer = "C:/temp/f2.bin";
+
+    cout << "Сервер запущен..." << endl;
+
+    long prevSize = 0;
+
+    while (true) {
+        ifstream in(fileRequest, ios::binary | ios::ate);
+        if (!in) {
+            Sleep(500);
+            continue;
+        }
+
+        long currSize = (long)in.tellg();
+
+        if (currSize > prevSize) {
+            in.seekg(prevSize);
+
+            Student s;
+            in.read((char*)&s, sizeof(Student));
+            prevSize = currSize;
+            in.close();
+
+            double ideal = (s.height - 100) * 0.9;
+            const char* result;
+
+            if (s.weight > ideal + 5)
+                result = "Избыточный вес";
+            else if (s.weight < ideal - 5)
+                result = "Недостаток веса";
+            else
+                result = "Норма";
+
+            cout << "Получен запрос: " << s.name << endl;
+
+            Student answer{};
+            strcpy_s(answer.name, sizeof(answer.name),result);
+
+            ofstream out(fileAnswer, ios::binary | ios::app);
+            out.write((char*)&answer, sizeof(Student));
+            out.close();
+        }
+
+        Sleep(500);
+    }
+
+    return 0;
+}
 }
