8c4f38c9cb819a6f427ffec481fe19a6bd60917f
s0195802m <margaritadocenko4@gmail.com>
2025-12-26 11:55:39 +0300

    Update ClientBinary.cpp

--- ClientBinary.cpp
+++ ClientBinary.cpp
@@ -1,130 +1,199 @@
-﻿#include <iostream>
-#include <fstream>
+#define _WINSOCK_DEPRECATED_NO_WARNINGS  // Чтобы inet_addr не ругался
+#include <winsock2.h>
 #include <windows.h>
-#include <cstring>
+#include <iostream>
+#include <thread>
+#include <string>
+
+#pragma comment(lib, "ws2_32.lib")
+
 using namespace std;
 
-struct Student {
-    char name[32];
-    int height;
-    int weight;
-};
+#define PORT 8888
+#define IP "127.0.0.1"
+#define BUF_SIZE 1024
 
-long getSize(const char* fname) {
-    ifstream f(fname, ios::binary | ios::ate);
-    if (!f) return 0;
-    return (long)f.tellg();
+SOCKET sock;
+bool running = true;
+
+// Поток для приёма сообщений
+void ReceiveThread() {
+    char buf[BUF_SIZE];
+    while (running) {
+        int bytes = recv(sock, buf, BUF_SIZE - 1, 0);
+        if (bytes > 0) {
+            buf[bytes] = '\0';
+            cout << buf << endl;
+        }
+        else {
+            running = false;
+        }
+    }
 }
 
 int main() {
     setlocale(LC_ALL, "rus");
 
-    const char* fileRequest = "C:/temp/f1.bin";
-    const char* fileAnswer = "C:/temp/f2.bin";
+    // Инициализация Winsock
+    WSADATA wsa;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+        cout << "Ошибка WSAStartup\n";
+        return 1;
+    }
 
-    cout << "Клиент запущен..." << endl;
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == INVALID_SOCKET) {
+        cout << "Ошибка сокета\n";
+        return 1;
+    }
+
+    sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(PORT);
+    addr.sin_addr.s_addr = inet_addr(IP); // теперь не ругается
+
+    if (connect(sock, (sockaddr*)&addr, sizeof(addr)) == SOCKET_ERROR) {
+        cout << "Не удалось подключиться к серверу\n";
+        return 1;
+    }
+
+    string name;
+    cout << "Введите ник: ";
+    getline(cin, name);
+
+    send(sock, name.c_str(), (int)name.size(), 0);
+
+    thread t(ReceiveThread); // запускаем поток для приёма сообщений
+
+    string msg;
+    while (running) {
+        getline(cin, msg);
+        if (!msg.empty()) send(sock, msg.c_str(), (int)msg.size(), 0);
+        if (msg == "/quit") break;
+    }
+
+    t.join();
+    closesocket(sock);
+    WSACleanup();
+
+    return 0;
+} 
+кдиент 
+#include <winsock2.h>
+#include <windows.h>
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <string>
+#include <mutex>
+
+#pragma comment(lib, "ws2_32.lib")
+
+using namespace std;
+
+#define PORT 8888
+#define BUF_SIZE 1024
+
+vector<SOCKET> clients;
+vector<string> names;
+mutex mtx;
+
+void SendAll(const string& msg, SOCKET skip = INVALID_SOCKET) {
+    lock_guard<mutex> lock(mtx);
+    for (auto client : clients) {
+        if (client != skip) send(client, msg.c_str(), (int)msg.size(), 0);
+    }
+}
+
+int FindUser(const string& name) {
+    for (size_t i = 0; i < names.size(); i++)
+        if (names[i] == name) return (int)i;
+    return -1;
+}
+
+void ClientThread(SOCKET sock) {
+    char buf[BUF_SIZE];
+    int bytes;
+
+    // Получаем ник
+    bytes = recv(sock, buf, BUF_SIZE - 1, 0);
+    if (bytes <= 0) { closesocket(sock); return; }
+    buf[bytes] = '\0';
+    string nickname = buf;
+
+    {
+        lock_guard<mutex> lock(mtx);
+        clients.push_back(sock);
+        names.push_back(nickname);
+    }
+
+    SendAll(nickname + " вошёл в чат", sock);
 
     while (true) {
-        Student s{};
+        bytes = recv(sock, buf, BUF_SIZE - 1, 0);
+        if (bytes <= 0) break;
+        buf[bytes] = '\0';
+        string msg = buf;
 
-        cout << "\nВведите имя: ";
-        cin >> s.name;
-        cout << "Введите рост: ";
-        cin >> s.height;
-        cout << "Введите вес: ";
-        cin >> s.weight;
+        if (msg == "/quit") break;
 
-        ofstream out(fileRequest, ios::binary | ios::app);
-        out.write((char*)&s, sizeof(Student));
-        out.close();
+        if (msg.find("/private ") == 0) {
+            string rest = msg.substr(9);
+            size_t pos = rest.find(' ');
+            if (pos != string::npos) {
+                string to = rest.substr(0, pos);
+                string text = rest.substr(pos + 1);
+                int id = FindUser(to);
+                if (id != -1) {
+                    string out = "Лично от " + nickname + ": " + text;
+                    send(clients[id], out.c_str(), (int)out.size(), 0);
+                }
+            }
+        }
+        else {
+            SendAll(nickname + ": " + msg, sock);
+        }
+    }
 
-        cout << "Запрос отправлен.\nОжидание ответа...\n";
-
-        long prevSize = getSize(fileAnswer);
-
-        while (true) {
-            Sleep(500);
-            long currSize = getSize(fileAnswer);
-            if (currSize > prevSize) {
-                ifstream in(fileAnswer, ios::binary);
-                in.seekg(prevSize);
-
-                Student ans{};
-                in.read((char*)&ans, sizeof(Student));
-                in.close();
-
-                cout << "Ответ сервера: " << ans.name << endl;
+    {
+        lock_guard<mutex> lock(mtx);
+        for (size_t i = 0; i < clients.size(); i++) {
+            if (clients[i] == sock) {
+                SendAll(names[i] + " вышел из чата");
+                closesocket(clients[i]);
+                clients.erase(clients.begin() + i);
+                names.erase(names.begin() + i);
                 break;
             }
         }
     }
-
-    return 0;
-
-
-    server 
-        #include <iostream>
-#include <fstream>
-#include <windows.h>
-#include <cstring>
-using namespace std;
-
-struct Student {
-    char name[32];
-    int height;
-    int weight;
-};
+}
 
 int main() {
-    setlocale(LC_ALL, "rus");
+    WSADATA wsa;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) { cout << "WSAStartup error\n"; return 1; }
 
-    const char* fileRequest = "C:/temp/f1.bin";
-    const char* fileAnswer = "C:/temp/f2.bin";
+    SOCKET server = socket(AF_INET, SOCK_STREAM, 0);
+    if (server == INVALID_SOCKET) { cout << "Socket error\n"; return 1; }
 
-    cout << "Сервер запущен..." << endl;
+    sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(PORT);
+    addr.sin_addr.s_addr = INADDR_ANY;
 
-    long prevSize = 0;
+    if (bind(server, (sockaddr*)&addr, sizeof(addr)) == SOCKET_ERROR) { cout << "Bind error\n"; return 1; }
+    if (listen(server, SOMAXCONN) == SOCKET_ERROR) { cout << "Listen error\n"; return 1; }
+
+    cout << "Server started on port " << PORT << endl;
 
     while (true) {
-        ifstream in(fileRequest, ios::binary | ios::ate);
-        if (!in) {
-            Sleep(500);
-            continue;
+        SOCKET client = accept(server, NULL, NULL);
+        if (client != INVALID_SOCKET) {
+            thread(ClientThread, client).detach();
         }
-
-        long currSize = (long)in.tellg();
-
-        if (currSize > prevSize) {
-            in.seekg(prevSize);
-
-            Student s;
-            in.read((char*)&s, sizeof(Student));
-            prevSize = currSize;
-            in.close();
-
-            double ideal = (s.height - 100) * 0.9;
-            const char* result;
-
-            if (s.weight > ideal + 5)
-                result = "Избыточный вес";
-            else if (s.weight < ideal - 5)
-                result = "Недостаток веса";
-            else
-                result = "Норма";
-
-            cout << "Получен запрос: " << s.name << endl;
-
-            Student answer{};
-            strcpy_s(answer.name, sizeof(answer.name),result);
-
-            ofstream out(fileAnswer, ios::binary | ios::app);
-            out.write((char*)&answer, sizeof(Student));
-            out.close();
-        }
-
-        Sleep(500);
     }
 
+    closesocket(server);
+    WSACleanup();
     return 0;
-}
 }
